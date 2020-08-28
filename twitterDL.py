20822c5015f0bf674a07c14149abbd8b2f99f899
Edward Bryson <edbryson7@gmail.com>
2020-08-28 13:02:25 -0400

    Removed Timing, used Tweepy's wait on api rate limit

--- twitterDL.py
+++ twitterDL.py
@@ -1,53 +1,70 @@
 import twitKeys as tk
 import tweepy
-import time
 import re
+import openpyxl
 
 
 def main():
+    # Authorizing the app with the twitter dev app API keys
     auth = tweepy.OAuthHandler(tk.API_KEY, tk.API_SECRET)
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+    # Regex for cleaning twitter's api data
     tweetAuthorRE = re.compile(r'\'([\d\w]+)\'')
     tweetLinkRE = re.compile(r'.*(https://t.co/[\w\d]*)')
 
+    # Excel file for storing and comparing tweet data
+    wb = openpyxl.load_workbook('twitLog.xlsx')
+    sheet = wb.active
+
     count = 0
-    # This variable hold the username or the userid of the user you want to get favorites from
-    # This needs to be the users unique username
+
     User = "@edzbrys"
     # Using cursor, return a generator of 1 million of the user's favorited tweets
-    for favorite in tweepy.Cursor(api.favorites, id=User).items(1000000):
-        # time variable used for delaying
-        now = time.time()
+    for favorite in tweepy.Cursor(api.favorites, id=User).items(5): #1000000):
 
-        #counter for number of tweets processed
+        # Counter for number of tweets processed
         count += 1
-        print(f'\n\nNumber: {count}')
 
-        # Regex searching of the tweet author and tweet link
-        authorRAW = str(favorite.user.screen_name.encode("utf-8"))
-        author = tweetAuthorRE.search(authorRAW)[1]
-        tweetText = str(favorite.text.encode("utf-8"))
-        tweetLink = tweetLinkRE.search(tweetText)[1]
-
-        tweetID = str(favorite.id)
-
-        # Prints tweet information
-        print(f'Author: {author}')
-        print(f'Id: {tweetID}')
-        print(f'Link: {tweetLink}')
-        print(
-            f'Date: {favorite.created_at.year}-{favorite.created_at.month}-{favorite.created_at.day}')
-
-        # Prints direct link of images within the tweet
         if 'media' in favorite.entities:
             for medium in favorite.extended_entities['media']:
                 if medium['type'] == 'photo':
-                    print('Tweet Image:', medium['media_url'])
 
-        # Delay to avoid going over twitter API rate limit
-        while(time.time() - now <= 3):
-            time.sleep(0.1)
+                    print(f'\n\nNumber: {count}')
+
+                    # Regex searching of the tweet author and tweet link
+                    authorRAW = str(favorite.user.screen_name.encode("utf-8"))
+                    tweetText = str(favorite.text.encode("utf-8"))
+                    try:
+                        tweetLink = tweetLinkRE.search(tweetText)[1]
+                    except:
+                        tweetLink = tweetText
+                    try:
+                        author = tweetAuthorRE.search(authorRAW)[1]
+                    except:
+                        author = authorRAW
+
+                    tweetID = str(favorite.id)
+                    tweetDate = f'{favorite.created_at.year}-{favorite.created_at.month}-{favorite.created_at.day}'
+
+                    # Prints tweet information
+                    print(f'Author: {author}')
+                    print(f'ID: {tweetID}')
+                    print(f'Link: {tweetLink}')
+                    print(f'Date: {tweetDate}')
+
+                    # Prints direct link of images within the tweet
+                    print('Image:', medium['media_url'])
+
+
+# Function to populate the excel file with twitter data from new tweets
+def writeExcel():
+    pass
+
+# Function to search Excel to see if excel 
+def searchExcel():
+    pass
+
 
 if __name__ == "__main__":
     main()
