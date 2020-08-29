2da0ddda2f378c1092f8e696d4b995d214688019
Edward Bryson <edbryson7@gmail.com>
2020-08-29 18:52:01 -0400

    Creation and removal of hyperlinks with openpyxl

--- twitterDL.py
+++ twitterDL.py
@@ -31,12 +31,14 @@ def main():
         print('Missing command line args')
         return
 
+    remove_hyperlink()
+
     db = pd.read_excel('twitDB.xlsx', dtype={'tweetID': str})
 
     rowsAppend = []
 
     # Using cursor, return a generator of 1 million of the user's favorited tweets
-    for favorite in tweepy.Cursor(api.favorites, id=User).items(1000000):
+    for favorite in tweepy.Cursor(api.favorites, id=User).items(20):
 
         # Counter for number of tweets processed
         tweetCount += 1
@@ -84,17 +86,22 @@ def main():
                 prevLoggedCount += 1
 
             # If there were 20 previously logged posts in a row, stop processing
-            if prevLoggedCount > 20:
+            if prevLoggedCount > 2000:
                 break
 
-            # Prints tweet information
 
-
+    # Add the new entries to the database
     db = db.append(rowsAppend)
+
+    # Sort the database by twitter ID
     db.sort_values(by=['tweetID'])
     print(db.head(1000000))
 
+    # Save the database
     db.to_excel('twitDB.xlsx', index=False)
+
+    # Using Openpyxl to convert plaintext to hyperlinks
+    create_hyperlink()
 
 
 # Function to populate the excel file with twitter data from new tweets
@@ -110,6 +117,38 @@ def write_db(rowsAppend, tweetID, author, date, tweetLink, photos):
 def search_db(db, tweetID):
     return tweetID in set(db["tweetID"])
 
+def remove_hyperlink():
+    wb = openpyxl.load_workbook('twitDB.xlsx')
+    sheet = wb.active
+
+    hyperlinkRE = re.compile(r'\("(.*)",')
+    for cell in list(sheet.columns)[3]:
+        linkSearch = hyperlinkRE.search(cell.value)
+        if linkSearch is not None:
+            cell.value = linkSearch[1]
+
+    for cell in list(sheet.columns)[4]:
+        linkSearch = hyperlinkRE.search(cell.value)
+        if linkSearch is not None:
+            cell.value = linkSearch[1]
+
+    wb.save('twitDB.xlsx')
+    wb.close()
+def create_hyperlink():
+    wb = openpyxl.load_workbook('twitDB.xlsx')
+    sheet = wb.active
+
+    # If the cell is not formatted as a hyperlink, do so
+    for cell in list(sheet.columns)[3]:
+        if cell.value[0] != '=':
+            cell.value = '=HYPERLINK("{}", "{}")'.format(cell.value, cell.value)
+    for cell in list(sheet.columns)[4]:
+        if cell.value[0] != '=':
+            cell.value = '=HYPERLINK("{}", "{}")'.format(cell.value, cell.value)
+
+
+    wb.save('twitDB.xlsx')
+    wb.close()
 
 if __name__ == "__main__":
     main()
